2f7390c30609eb6d9e60382179770697483c7843
kj2138998670 <haonan.wang@my.jcu.edu.au>
2018-11-20 13:52:17 +0800

    input name

--- oddname.py
+++ oddname.py
@@ -1,8 +1,20 @@
 """input a name and print alternate characters"""
 def main():
-name=input("enter name")
-# error check for name to be blank
-while len(name)<1:
-    name=input("enter name")
-print(name[::2])
+    name = get_name()
+    num=int(input("enter the skip value"))
+    print_name(name)
+
+
+def print_name(name):
+    print(name[::2])
+
+
+def get_name():
+    name = input("enter name")
+    # error check for name to be blank
+    while len(name) < 1:
+        name = input("enter name")
+    return name
+
+
 main()
