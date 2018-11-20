28600cdd178ee9c12a84ee4b7b82d2b8ab95ac81
kj2138998670 <haonan.wang@my.jcu.edu.au>
2018-11-20 13:47:40 +0800

    input name

--- oddname.py
+++ oddname.py
@@ -1,6 +1,7 @@
 """input a name and print alternate characters"""
 def main():
 name=input("enter name")
+# error check for name to be blank
 while len(name)<1:
     name=input("enter name")
 print(name[::2])
